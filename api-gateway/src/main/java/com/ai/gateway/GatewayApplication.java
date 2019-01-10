3d2877e456f2301c9bcf1bf5a664f7029d2b8260
luocongqiu <luocongqiu@gmail.com>
2019-01-10 18:33:04 +0800

    权限修改

--- api-gateway/src/main/java/com/ai/gateway/GatewayApplication.java
+++ api-gateway/src/main/java/com/ai/gateway/GatewayApplication.java
@@ -3,8 +3,10 @@ package com.ai.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringCloudApplication
+@ComponentScan("com.ai")
 @EnableZuulProxy
 public class GatewayApplication {
 
